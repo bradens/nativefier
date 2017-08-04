0904a851903a78c6cf5861484165d4a0f363eb57
Braden Simpson <braden@uvic.ca>
2017-08-04 09:05:09 -0600

    Allow dynamic url / installer

--- release_coar.sh
+++ release_coar.sh
@@ -1,16 +1,29 @@
 #! /bin/zsh
 
+domain="https://coar.io"
+name="Coar"
+
+if [ ! -z "$1" ]
+  then
+    domain=$1
+fi
+
+if [ ! -z "$2" ]
+  then
+    name=$2
+fi
+
 echo "build nativefier deps"
 npm run dev-up && npm link
 
 echo "Create apps"
-nativefier "https://coar.io" -n Coar -p win32 -e 1.6.5 ~/Dropbox/CoarApps &
-nativefier "https://coar.io" -n Coar -e 1.6.5 ~/Dropbox/CoarApps &
-nativefier "https://coar.io" -n Coar -p linux -e 1.6.5 ~/Dropbox/CoarApps &
+nativefier $domain -n $name -p win32 -e 1.6.5 ~/Dropbox/CoarApps &
+nativefier $domain -n $name -e 1.6.5 ~/Dropbox/CoarApps &
+nativefier $domain -n $name -p linux -e 1.6.5 ~/Dropbox/CoarApps &
 wait
 
 echo "Creating windows installer"
 
-./create_windows_installer
+./create_windows_installer $name
 
 echo "Done creating apps"
